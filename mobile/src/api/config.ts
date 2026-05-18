1b7b412e365864807e2087de28002d1be1283b4e
Trần Vũ <tranv334@gmail.com>
2026-05-18 14:26:17 +0700

    update config

--- mobile/src/api/config.ts
+++ mobile/src/api/config.ts
@@ -3,7 +3,11 @@ import { Platform } from 'react-native';
 // Kairos Garden backend chạy ở port 5000 (HTTP)
 const API_PORT = 5000;
 
-// For now, dev focus on physical device over WiFi
-const API_HOST = '192.168.1.180';
+// Emulator
+const API_HOST = '10.0.2.2';
+
+// Physical device 
+// const API_HOST = '192.168.1.180';
+
 
 export const API_BASE_URL = `http://${API_HOST}:${API_PORT}`;
